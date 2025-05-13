f8a7ccc2799afae058d831e7b971d37cf1ac488d
TepavacAndrija <andrijatepavacskola@gmail.com>
2025-05-13 14:31:08 +0200

    Security update: rate limiting and bot protection

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,47 @@ app.use(cors());
 app.use(helmet())
 app.use(morgan("dev"))
 
+// apply arcjet rate-limit to every route
+
+app.use(async (req,res) => {
+    try {
+        const decision = await aj.protect (req, {
+            requested:1
+        })
+
+        if(decision.isDenied()){
+            if(decision.reason.isRateLimit()){
+                res.status(429).json({
+                    error: "Too Many Requests"
+                })
+            }
+            else if (decision.reason.isBot()){
+                res.status(403).json({
+                    error:"Bot Acces Denied"
+                })
+            }
+            else{
+                res.status(403).json({
+                    error:"Forbidden"
+                })
+            }
+            return
+        }
+
+        if (decision.results.some((result) => result.reason.isBot() && result.reason.isSpoofed())){
+            res.status(403).json({
+                    error:"Spoofed Bot"
+                })
+        }
+
+        next()
+        
+    } catch (error) {
+        console.log("Error while applying arcjet",error)
+        next(error)
+    }
+})
+
 app.use("/api/products", productRoutes)
 
 async function initDB(){
