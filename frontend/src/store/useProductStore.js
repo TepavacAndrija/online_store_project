76eca20197832b07cd834703d2b1768fd8be8d7f
TepavacAndrija <andrijatepavacskola@gmail.com>
2025-05-14 02:32:28 +0200

    Update: added product page

--- frontend/src/store/useProductStore.js
+++ frontend/src/store/useProductStore.js
@@ -69,7 +69,7 @@ export const useProductStore = create((set, get) => ({
       set({ loading: false });
     }
   },
-
+  //get product
   fetchProduct: async (id) => {
     set({ loading: true });
     try {
@@ -86,6 +86,7 @@ export const useProductStore = create((set, get) => ({
       set({ loading: false });
     }
   },
+  //update product
   updateProduct: async (id) => {
     set({ loading: true });
     try {
